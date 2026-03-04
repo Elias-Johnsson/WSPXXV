3dafea205cb5f64ffff0d0222352631149615fd5
Elias-johnsson <elias.johnsson5@outlook.com>
2026-03-04 16:08:31 +0100

    h

--- app.rb
+++ app.rb
@@ -3,3 +3,23 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+require_relative 'model.rb'
+
+
+get('/trava') do
+  db = db()
+  slim(:start)
+end
+
+get('/login') do
+  db = db()
+  slim(:login)
+end
+
+post('/login') do
+  
+end
+
+get('/signin') do
+  slim(:signin)
+end
