217827829b34344223ad7eb5fa2a684e9c966d26
Elias-johnsson <elias.johnsson5@outlook.com>
2026-03-11 16:23:06 +0100

    fixing database and login/sign in

--- app.rb
+++ app.rb
@@ -5,21 +5,56 @@ require 'sinatra/reloader'
 require 'bcrypt'
 require_relative 'model.rb'
 
-
 get('/trava') do
-  db = db()
   slim(:start)
 end
 
+get('/trava/home') do
+  db = db()
+  id = params["id"].to_i
+  @items = db.execute("SELECT * FROM groups WHERE member_id=?",)
+  slim(:home)
+end
 get('/login') do
   db = db()
   slim(:login)
 end
 
 post('/login') do
-  
+  user = params["username"]
+  pwd = params["password"]
+  result=db.execute("SELECT id, pwd_digest FROM users WHERE name=?",user)
+  if result.empty?
+    redirect('/error')
+  end
+  used_id = result.first["id"]
+  pwd_digest = result.first["pwd_digest"]
+  if BCrypt::Password.new(pwd_digest) == pwd
+    session[:user_id] = used_id
+    redirect('/trava/home')
+  else
+    redirect('/login')
+  end
 end
 
 get('/signin') do
   slim(:signin)
+end
+
+post('/signin') do
+  user = params["username"]
+  pwd = params["password"]
+  pwd_confirm = params["pwd_confirm"]
+  result = db.execute("SELECT id FROM users WHERE name=?",user)
+  if result.empty?
+    if pwd == pwd_confirm
+      pwd_digest = BCrypt::Password.create(pwd)
+      db.execute("INSERT INTO users(name,pwd_digest) VALUES(?,?)",[user,pwd_digest])
+      redirect("/trava/home")  
+    else
+      redirect('/error')
+    end
+  else
+    redirect('/login')
+  end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,10 +1,10 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new("databas.db")
+db = SQLite3::Database.new("workouts.db")
 
 
 def seed!(db)
-  puts "Using db file: db/todos.db"
+  puts "Using db file: db/workouts.db"
   puts "🧹 Dropping old tables..."
   drop_tables(db)
   puts "🧱 Creating tables..."
@@ -18,29 +18,36 @@ def drop_tables(db)
   db.execute('DROP TABLE IF EXISTS users')
   db.execute('DROP TABLE IF EXISTS posts')
   db.execute('DROP TABLE IF EXISTS groups')
+  db.execute('DROP TABLE IF EXISTS memberships')
 end
 
 def create_tables(db)
   db.execute('CREATE TABLE users (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
+              name TEXT NOT NULL,
               pwd_digest TEXT NOT NULL)')
+
   db.execute('CREATE TABLE posts (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               title TEXT NOT NULL,
               desc TEXT NOT NULL,
               group_id INTEGER,
-              duration TEXT NOT NULL)')
-  db.execute('CREATE TABLE groups(
+              user_id INTEGER)')
+
+  db.execute('CREATE TABLE groups (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL,
-              members_id INTEGER)')
+              name TEXT NOT NULL)')
+
+  db.execute('CREATE TABLE memberships(
+              user_id INTEGER,
+              group_id INTEGER)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO users (id,name,pwd_digest) VALUES (1,"Admin", "benis")')
-  db.execute('INSERT INTO posts (id, title, desc,group_id,duration) VALUES (1,"BENIS", "En rödgran",1,"20 minuter")')
-  db.execute('INSERT INTO groups (id,name, members_id) VALUES (1,"Pynta gran", 1)')
+  db.execute('INSERT INTO users (id,name,pwd_digest) VALUES (1,"test", "benis")')
+  db.execute('INSERT INTO posts (id, title, desc,group_id,user_id) VALUES (1,"BENIS", "En rödgran",1,1)')
+  db.execute('INSERT INTO groups (id,name) VALUES (1,"Pynta gran")')
+  db.execute('INSERT INTO memberships (user_id,group_id) VALUES (1,1)')
 end
 
 
